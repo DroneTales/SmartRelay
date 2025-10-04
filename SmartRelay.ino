af8ee3aba1af0adcafca1afa4b9036868ef8db56
Mike Petrichenko <mike@btframework.com>
2025-10-04 11:29:23 +0300

    Update SmartRelay.ino

--- SmartRelay.ino
+++ SmartRelay.ino
@@ -33,17 +33,14 @@
 /*                                  HomeSpan sensors                                  */
 
 // Lamp relay record.
-struct LampRelay : Service::LightBulb
-{
+struct LampRelay : Service::LightBulb {
     SpanCharacteristic* FPower;
     
-    LampRelay() : Service::LightBulb()
-    {
+    LampRelay() : Service::LightBulb() {
         FPower = new Characteristic::On();
     }
     
-    bool update()
-    {
+    bool update() {
         digitalWrite(RELAY_PIN, FPower->getNewVal());
         return true;
     }
@@ -56,8 +53,7 @@ struct LampRelay : Service::LightBulb
 /*                                 Arduino  rountines                                 */
 
 // Arduino initialization routine.
-void setup()
-{
+void setup() {
     // Initialize debug serial port.
     Serial.begin(115200);
 
@@ -92,8 +88,7 @@ void setup()
 }
 
 // Arduino main loop.
-void loop()
-{
+void loop() {
     homeSpan.poll();
 }
 
